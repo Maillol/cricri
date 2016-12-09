8a38ebd0069b4e26048c0d7491008bf24b8dfbb6
Vincent Maillol <vincent.maillol@gmail.com>
2016-12-09 10:22:26 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='gentest',
+    version='0.1',
+    description='Scenario test generator',
+    keywords='scenario test generator unittest case',
+    author='Vincent Maillol',
+    author_email='vincent.maillol@gmail.com',
+    url='https://github.com/maillol/scenario',
+    license='GPLv3', 
+    py_modules=['gentest'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    ]
+)
